78e8ded1770b2fb84b40458b2a699db464e6f49d
Michael Symmes <msymmes@gmail.com>
2015-05-27 19:04:36 -0400

    added SongQueue methods

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,10 +2,29 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', this.enqueue, this)
+        .on('dequeue', this.dequeue, this)
+        .on('ended', this.ended, this);
+  },
+
+  enqueue: function(){
+    if (this.models.length === 1){ this.playFirst(); }
+  },
+
+  dequeue: function(){
+    this.shift();
+  },
+
+  ended: function(){
+    this.dequeue();
+    if (this.models.length){
+      this.playFirst();
+    }
   },
 
   playFirst: function(){
-
+    this.at(0).play();
+    console.log('IM PLAYING THE FIRST THING');
   }
 
 });
